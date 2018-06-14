2116db896525479c2e4b7d2335be0528cde1a7a3
mtaddevi <mtaddevi@gmail.com>
2018-06-13 22:47:24 -0400

    add early project

---
+++ Mowio/Assets/scripts/mowerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mowerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+    //private int count;
+    //public Text countText;
+    // Text winText;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //count = 0;
+       // setCountText();
+        //winText.text = "";
+    }
+
+    //physics in fixed
+    private void FixedUpdate()
+    {
+        float moveHoriz = Input.GetAxis("Horizontal");
+        float moveVert = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHoriz, 0.0f, moveVert);
+
+        rb.AddForce(movement * speed);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            //count = count + 1;
+            //setCountText();
+
+        }
+    }
+
+ //   void setCountText()
+  //  {
+//countText.text = "Count: " + count.ToString();
+   //    if (count >= 17)
+//{
+     //       winText.text = "YOU WIN!";
+//}
+    //}
+
+}
